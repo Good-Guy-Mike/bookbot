b30063baefd431fb8ad76735d15fa6d115b4dba1
Good-Guy-Mike <michal.zielinski89@tlen.pl>
2024-11-22 15:28:39 +0100

    final touches

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    num_letters = get_num_letters(text)
-    sorted_list = letter_sorter(num_letters)
+    num_letters = get_num_freq(text)
     print("--- Begin report of books/frankenstein.txt ---")
     print(f"{num_words} words found in the document")
-    letter_report(sorted_list)
+    report_letter_counts(num_letters)
     print("--- End report ---")
     
 
@@ -14,7 +13,7 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_num_letters(text):
+def get_num_freq(text):
     letter_count = {}
     lowered_string = text.lower()
     for letter in lowered_string:
@@ -24,6 +23,22 @@ def get_num_letters(text):
             letter_count[letter] = 1
     return letter_count
 
+def report_letter_counts(letter_count):
+    sorted_letters = sorted(letter_count.items(), key=lambda x: x[1], reverse=True)
+    for letter, count in sorted_letters:
+        print(f"The '{letter}' character was found {count} times")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
+
+
+
+
+"""
 def letter_sorter(num_letters):
     list_of_letters = []
     for letter in num_letters:
@@ -37,11 +52,4 @@ def letter_report(sorted_list):
         for letter in item:  
             number = item[letter]  
             print(f"The '{letter}' character was found {number} times")
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-
-main()
+"""
